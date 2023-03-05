dba6f98ff181121b859ae500b67fd93b0103736c
matheuswilliam01 <matheuswilliam2002@hotmail.com>
2023-03-05 00:08:45 -0300

    Addition of fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+type Produto = {
+  nomeProduto: string,
+  valor: number,
+  descricao: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'product-registration';
+  constructor() { }
+
+  lista: Produto[] = [];
+
+  public adicionarProduto(nomeProduto: string, valor: string, descricao: string): void {
+    this.lista.push({
+      nomeProduto: nomeProduto,
+      valor: parseInt(valor),
+      descricao: descricao,
+    });
+  }
 }
